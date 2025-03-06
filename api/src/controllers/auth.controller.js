4574c5bb59a0ac15afdcd67bdcb7bd7917dbbb24
23-faisal <a.faisal23r@gmail.com>
2025-03-06 18:55:55 +0600

    create login functionality in the backend

--- api/src/controllers/auth.controller.js
+++ api/src/controllers/auth.controller.js
@@ -1,12 +1,21 @@
 import { z } from "zod";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import "dotenv/config";
 
 // Define a schema using Zod
 const registerSchema = z.object({
   username: z
     .string()
     .min(3, { message: "Username must be at least 3 characters long" }),
+  email: z.string().email({ message: "Invalid email format" }),
+  password: z
+    .string()
+    .min(6, { message: "Password must be at least 6 characters long" }),
+});
+
+const loginSchema = z.object({
   email: z.string().email({ message: "Invalid email format" }),
   password: z
     .string()
@@ -47,7 +56,50 @@ const registerController = async (req, res) => {
 };
 
 const loginController = async (req, res) => {
-  res.json({ message: "Login route" });
+  try {
+    const { email, password } = loginSchema.parse(req.body);
+
+    const userExists = await User.findOne({
+      email,
+    });
+
+    // if user does not exist
+
+    if (!userExists) {
+      return res.status(400).json({ message: "User does not exist" });
+    }
+
+    // check password
+
+    const isPasswordValid = await bcrypt.compare(password, userExists.password);
+
+    // if password is invalid
+
+    if (!isPasswordValid) {
+      return res.status(400).json({ message: "Invalid password" });
+    }
+
+    // token
+
+    const token = jwt.sign({ id: userExists._id }, process.env.JWT_SECRET, {expiresIn: "24h"});
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      maxAge: 24 * 60 * 60 * 1000, // 24 hours
+    });
+    
+
+    res.status(200).json({
+      success: true,
+      message: "User logged in successfully!",
+      token,
+      username: userExists.username,
+      email: userExists.email,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
 };
 
 export { registerController, loginController };
--- api/src/routes/auth.route.js
+++ api/src/routes/auth.route.js
@@ -4,11 +4,13 @@ import {
   registerController,
 } from "../controllers/auth.controller.js";
 
+import authMiddleware from "../middlewares/userValidation.js";
+
 const authRouter = Router();
 
 // prefix : api/auth
 authRouter.post("/register", registerController);
 
-authRouter.post("/login", loginController);
+authRouter.post("/login", authMiddleware, loginController);
 
 export default authRouter;
