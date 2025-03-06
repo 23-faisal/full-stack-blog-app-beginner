582666fc94433ac04435749b7b26f8dc69174d7a
23-faisal <a.faisal23r@gmail.com>
2025-03-06 15:40:24 +0600

    create register page in the ui

--- client/src/pages/RegisterPage.tsx
+++ client/src/pages/RegisterPage.tsx
@@ -1,9 +1,88 @@
- 
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+const formSchema = z.object({
+  username: z
+    .string()
+    .min(3, { message: "Username must be at least 3 characters" }),
+  email: z.string().email({ message: "Invalid email format" }),
+  password: z
+    .string()
+    .min(6, { message: "Password must be at least 6 characters" }),
+});
+
+type FormData = z.infer<typeof formSchema>;
 
 const RegisterPage = () => {
-  return (
-    <div>RegisterPage</div>
-  )
-}
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<FormData>({
+    resolver: zodResolver(formSchema),
+  });
 
-export default RegisterPage
+  const onSubmit = async (data: FormData) => {
+    console.log("Register Data:", data);
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+  };
+  return (
+    <div className="max-w-md mx-auto mt-10 p-6 border rounded-lg shadow-lg">
+      <h2 className="text-2xl font-bold mb-4 text-center">Register</h2>
+      <form onSubmit={handleSubmit(onSubmit)} action="" className="space-y-4 ">
+        {/* Username input */}
+        <div>
+          <Label htmlFor="username">Username</Label>
+          <Input
+            id="username"
+            type="text"
+            {...register("username")}
+            className="mt-1 w-full"
+          />
+          {errors?.username && (
+            <p className="text-red-500">{errors.username.message}</p>
+          )}
+        </div>
+
+        {/* Email Input */}
+        <div>
+          <Label htmlFor="email">Email</Label>
+          <Input
+            id="email"
+            type="email"
+            {...register("email")}
+            className="mt-1 w-full"
+          />
+          {errors.email && (
+            <p className="text-red-500">{errors.email.message}</p>
+          )}
+        </div>
+
+        {/*  Password Input */}
+        <div>
+          <Label htmlFor="password">Password</Label>
+          <Input
+            id="password"
+            type="password"
+            {...register("password")}
+            className="mt-1 w-full"
+          />
+          {errors.password && (
+            <p className="text-red-500">{errors.password.message}</p>
+          )}
+        </div>
+
+        {/*  Submit Button */}
+        <Button type="submit" className="w-full" disabled={isSubmitting}>
+          {isSubmitting ? "wait a sec..." : "Register"}
+        </Button>
+      </form>
+    </div>
+  );
+};
+
+export default RegisterPage;
